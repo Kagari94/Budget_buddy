7a0822f4aee8b18a14ae649124c53152ebd06bb6
laovirtanen <laurivirtanen@outlook.com>
2024-12-10 13:53:28 +0200

    theme and settings page applied without styling

--- components/settings/CurrencyPicker.js
+++ components/settings/CurrencyPicker.js
@@ -108,7 +108,6 @@ const CurrencyPicker = ({ onCurrencySelect }) => {
 
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
       paddingHorizontal: 20,
