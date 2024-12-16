539705aefecfb70ebf1e0860c805a7d1515a0992
Kagari94 <jesse.hirvo@hotmail.com>
2024-12-16 13:19:12 +0200

    updated icons

--- components/category_icons/CategoryIcons.js
+++ components/category_icons/CategoryIcons.js
@@ -1,11 +1,11 @@
 export const buttons = [//Icon list for now
     {id: 'All', icon: 'all-inclusive', color: 'black', size: 20},
     { id: 'Food', icon: 'food', color: 'black', size: 20 },
-    { id: 'Living', icon: 'currency-eur', color: 'black', size: 20 },
+    { id: 'Living', icon: 'home-heart', color: 'black', size: 20 },
     { id: 'Transport', icon: 'bus', color: 'black', size: 20 },
-    { id: 'Hobbies', icon: 'teddy-bear', color: 'black', size: 20 },
+    { id: 'Hobbies', icon: 'tennis', color: 'black', size: 20 },
     { id: 'Travel', icon: 'bag-suitcase-outline', color: 'black', size: 20 },
-    { id: 'Savings', icon: 'teddy-bear', color: 'black', size: 20 },
-    { id: 'Others', icon: 'asterisk', color: 'black', size: 20 },
+    { id: 'Savings', icon: 'piggy-bank', color: 'black', size: 20 },
+    { id: 'Others', icon: 'dots-horizontal-circle', color: 'black', size: 20 },
     { id: 'Income', icon: 'wallet-plus', color: 'black', size: 20 },
 ]
