33d5ca60f1682123df16a2a702737efd42f42075
NicoK81 <n2kyni00@students.oamk.fi>
2024-12-16 17:34:35 +0200

    slider fix

--- screens/investing_hint/InvestingHint.js
+++ screens/investing_hint/InvestingHint.js
@@ -185,7 +185,6 @@ export default function InvestingHint() {
               minimumValue={1}
               maximumValue={50}
               step={1}
-              value={investmentYears}
               onValueChange={(value) => setInvestmentYears(value)}
               minimumTrackTintColor={colors.primary}
               thumbTintColor={colors.primary}
